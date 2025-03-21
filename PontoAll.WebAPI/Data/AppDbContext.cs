e3ba21a63a39af75a7bb67e1404ecf67d535b369
Larksel <lemuelgames@outlook.com>
2025-03-21 17:15:45 -0300

    :sparkles: feat: Criado AppDbContext

---
+++ PontoAll.WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PontoAll.WebAPI.Data.Builders;
+using PontoAll.WebAPI.Objects.Models;
+
+namespace PontoAll.WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Scale> Scales { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            ScaleBuilder.Build(modelBuilder);
+        }
+    }
+}
