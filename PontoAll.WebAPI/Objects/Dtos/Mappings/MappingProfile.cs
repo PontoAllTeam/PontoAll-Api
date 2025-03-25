9061e81d429f5acb2eeb8dfc87e888a2788b5ffc
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-03-25 14:50:41 -0300

    :sparkles: feat: Criando mapeação da classe Company

---
+++ PontoAll.WebAPI/Objects/Dtos/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PontoAll.WebAPI.Objects.Dtos.Entities;
+using PontoAll.WebAPI.Objects.Models;
+
+namespace PontoAll.WebAPI.Objects.Dtos.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CompanyDTO, Company>().ReverseMap();
+        }
+    }
+}
