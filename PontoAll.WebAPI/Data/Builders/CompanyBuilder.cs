d9fbd02495f61945805b38d95cdb1a890017b0bb
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-03-25 22:09:59 -0300

    :sparkles: feat: Criando Builder da classe Company

---
+++ PontoAll.WebAPI/Data/Builders/CompanyBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PontoAll.WebAPI.Objects.Models;
+using System.Threading;
+
+namespace PontoAll.WebAPI.Data.Builders;
+
+public class CompanyBuilder
+{
+    public static void Build(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Company>().HasKey(s => s.Id);
+        modelBuilder.Entity<Company>().Property(s => s.CorporateName).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Company>().Property(s => s.FantasyName).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Company>().Property(s => s.Cnpj).IsRequired().HasMaxLength(14);
+        modelBuilder.Entity<Company>().Property(s => s.Email).IsRequired().HasMaxLength(50);
+        modelBuilder.Entity<Company>().Property(s => s.BusinessPhone).IsRequired().HasMaxLength(11);
+        modelBuilder.Entity<Company>().Property(s => s.State).IsRequired().HasMaxLength(2);
+        modelBuilder.Entity<Company>().Property(s => s.City).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Company>().Property(s => s.Cep).IsRequired().HasMaxLength(8);
+        modelBuilder.Entity<Company>().Property(s => s.Street).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Company>().Property(s => s.Neighborhood).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<Company>().Property(s => s.Number).IsRequired().HasMaxLength(5);
+        modelBuilder.Entity<Company>().Property(s => s.Status).IsRequired();
+    }
+}
