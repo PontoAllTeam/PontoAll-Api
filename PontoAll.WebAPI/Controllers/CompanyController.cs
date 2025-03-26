cddaeb911c4cd33e6558c3fe44a503e67511c2f8
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-03-26 15:40:13 -0300

    :sparkles: feat: Adicionando configurações

--- PontoAll.WebAPI/Controllers/CompanyController.cs
+++ PontoAll.WebAPI/Controllers/CompanyController.cs
@@ -25,7 +25,7 @@ namespace PontoAll.WebAPI.Controllers
             return Ok(companies);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] 
         public async Task<IActionResult> GetById(int id)
         {
             var companies = await _companyService.GetById(id);
