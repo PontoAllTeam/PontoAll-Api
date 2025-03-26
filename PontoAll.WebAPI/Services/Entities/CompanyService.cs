85cb4871512eb85ba9efbd9881a908b0affc9088
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-03-25 22:34:28 -0300

    :sparkles: feat: Criando Services da classe Company

---
+++ PontoAll.WebAPI/Services/Entities/CompanyService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PontoAll.WebAPI.Data.Interfaces;
+using PontoAll.WebAPI.Objects.Models;
+using PontoAll.WebAPI.Services.Interfaces;
+
+namespace PontoAll.WebAPI.Services.Entities;
+
+public class CompanyService : GenericService<Company>, ICompanyService
+{
+    private readonly ICompanyRepository _companyRepository;
+    private readonly IMapper _mapper;
+
+    public CompanyService(ICompanyRepository repository, IMapper mapper) : base(repository, mapper)
+    {
+        _companyRepository = repository;
+        _mapper = mapper;
+    }
+
+}
