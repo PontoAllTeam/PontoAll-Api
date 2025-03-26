dda66301e5b1a2b281b6170b4ee03adcd3cbf158
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-03-26 15:28:21 -0300

    :sparkles: feat: Adicionando CompanyBuilder e DbSet Companies

--- PontoAll.WebAPI/Data/AppDbContext.cs
+++ PontoAll.WebAPI/Data/AppDbContext.cs
@@ -9,11 +9,13 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Scale> Scales { get; set; }
+    public DbSet<Company> Companies { get; set; } 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         ScaleBuilder.Build(modelBuilder);
+        CompanyBuilder.Build(modelBuilder);
     }
 }
