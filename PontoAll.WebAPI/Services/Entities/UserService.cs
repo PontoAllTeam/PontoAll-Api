1471bb4bfd0755539e87d1562e1bda4762cff059
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-26 21:34:17 -0300

    :sparkles: feat: Criando UserService

---
+++ PontoAll.WebAPI/Services/Entities/UserService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PontoAll.WebAPI.Data.Interfaces;
+using PontoAll.WebAPI.Objects.Models;
+using PontoAll.WebAPI.Services.Interfaces;
+
+namespace PontoAll.WebAPI.Services.Entities;
+
+public class UserService : GenericService<User>, IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserService(IUserRepository repository, IMapper mapper) : base(repository, mapper)
+    {
+        _userRepository = repository;
+        _mapper = mapper;
+    }
+}
