8a69174f668284d06c34713e0a00cc8ad5abd2d7
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-01 20:24:17 -0300

    :sparkles: feat: Criando DepartmentRepository

---
+++ PontoAll.WebAPI/Data/Repositories/DepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using PontoAll.WebAPI.Objects.Models;
+using PontoAll.WebAPI.Data.Interfaces;
+
+namespace PontoAll.WebAPI.Data.Repositories;
+
+public class DepartmentRepository : GenericRepository<Department>,IDepartmentRepository
+{
+    private readonly AppDbContext _context;
+    public DepartmentRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
