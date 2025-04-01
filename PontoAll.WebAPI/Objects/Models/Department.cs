78b93c8c009f1e15baf9b84013031b830f1e1ff3
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-01 14:58:22 -0300

    :sparkles: feat: Criando classe Department

---
+++ PontoAll.WebAPI/Objects/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PontoAll.WebAPI.Objects.Models;
+
+[Table("department")]
+public class Department
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    public Department() { }
+    public Department(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+}
