298b55646293c4aa3488d833dba0d84e30152a6e
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-03 21:20:39 -0300

    :sparkles: feat: Criando classe Sector

---
+++ PontoAll.WebAPI/Objects/Models/Sector.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PontoAll.WebAPI.Objects.Models;
+
+[Table("sector")]
+public class Sector
+{
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("name")] 
+    public string Name { get; set; }
+
+    public Sector() { }
+    public Sector(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
