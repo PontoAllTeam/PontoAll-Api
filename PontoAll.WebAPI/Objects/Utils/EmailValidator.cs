736c2bd65700c3bda29cf4755661f77c34f4899d
Larksel <lemuelgames@outlook.com>
2025-04-04 13:16:32 -0300

    :recycle: refactor: Função de verificação de email melhorada

--- PontoAll.WebAPI/Objects/Utils/EmailValidator.cs
+++ PontoAll.WebAPI/Objects/Utils/EmailValidator.cs
@@ -2,22 +2,21 @@ namespace PontoAll.WebAPI.Objects.Utils;
 
 public class EmailValidator
 {
-    public static int CheckValidEmail(string email)
+    public static bool IsValidEmail(string email)
     {
-        int atCount = email.Count(c => c == '@');
-        bool hasTextBeforeAt = email.IndexOf('@') > 0;
-        bool hasTextAfterAt = email.LastIndexOf('@') < email.Length - 1;
-
         int atPosition = email.IndexOf('@');
+        int atCount = email.Count(c => c == '@');
+
+        bool hasTextBeforeAt = atPosition > 0;
+        bool hasTextAfterAt = email.LastIndexOf('@') < email.Length - 1;
         bool hasDotAfterAt = atPosition >= 0 && email.IndexOf('.', atPosition) > atPosition;
         bool endsWithDot = email.EndsWith('.');
+        bool hasSingleAt = atCount != 1;
 
-        if (atCount != 1) return -1;
-        else if (!hasTextBeforeAt) return -1;
-        else if (!hasTextAfterAt) return -2;
-        else if (!hasDotAfterAt) return -2;
-        else if (endsWithDot) return -1;
-
-        return 1;
+        return hasSingleAt ||
+               hasTextBeforeAt ||
+               hasTextAfterAt ||
+               hasDotAfterAt ||
+               !endsWithDot;
     }
 }
