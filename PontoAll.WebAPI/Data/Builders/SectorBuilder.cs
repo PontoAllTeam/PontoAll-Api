1e03542e4049b996383b87edf0896066ae8a2dbf
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-03 21:51:28 -0300

    :sparkles: feat: Criando SectorBuilder

---
+++ PontoAll.WebAPI/Data/Builders/SectorBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PontoAll.WebAPI.Objects.Models;
+
+namespace PontoAll.WebAPI.Data.Builders;
+
+public class SectorBuilder
+{
+    public static void Builder(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sector>().HasKey(s => s.Id);
+        modelBuilder.Entity<Sector>().Property(s => s.Name).IsRequired().HasMaxLength(100);
+    }
+}
