dbdfc1742899f3d83defe0e0018355c6108eb197
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-04 21:33:09 -0300

    :sparkles: feat: Criando SectorDTO

--- PontoAll.WebAPI/Objects/Dtos/Entities/SectorDTO.cs
+++ PontoAll.WebAPI/Objects/Dtos/Entities/SectorDTO.cs
@@ -3,5 +3,7 @@
 public class SectorDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }    
+    public string Name { get; set; }   
+    public int DepartmentId { get; set; }
+
 }
