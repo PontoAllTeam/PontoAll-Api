63dcdbe08dee33ffb0ec4090a91054fb691bd867
Larksel <lemuelgames@outlook.com>
2025-05-19 00:43:00 -0300

    :recycle: refactor: Anotações de Geolocation removidas

--- PontoAll.WebAPI/Objects/Contracts/Geolocation.cs
+++ PontoAll.WebAPI/Objects/Contracts/Geolocation.cs
@@ -1,13 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PontoAll.WebAPI.Objects.Contracts;
 
 public class Geolocation
 {
-    [Range(-90, 90, ErrorMessage = "Latitude deve estar entre -90 e 90 graus.")]
     public double Latitude { get; set; }
-
-    [Range(-180, 180, ErrorMessage = "Longitude deve estar entre -180 e 180 graus.")]
     public double Longitude { get; set; }
 
     public Geolocation() {}
