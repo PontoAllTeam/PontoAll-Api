aeb1cf4eb209c3e29fa8fa2caaa95b4d84306b22
Larksel <lemuelgames@outlook.com>
2025-05-18 22:45:31 -0300

    :sparkles: feat: Adicionado construtor à Geolocation e anotações indicando os limites dos valores

--- PontoAll.WebAPI/Objects/Contracts/Geolocation.cs
+++ PontoAll.WebAPI/Objects/Contracts/Geolocation.cs
@@ -1,7 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PontoAll.WebAPI.Objects.Contracts;
 
 public class Geolocation
 {
+    [Range(-90, 90, ErrorMessage = "Latitude deve estar entre -90 e 90 graus.")]
     public double Latitude { get; set; }
+
+    [Range(-180, 180, ErrorMessage = "Longitude deve estar entre -180 e 180 graus.")]
     public double Longitude { get; set; }
+
+    public Geolocation() {}
+
+    public Geolocation(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
 }
