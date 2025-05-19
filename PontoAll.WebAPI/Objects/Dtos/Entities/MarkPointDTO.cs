79b03d0035e0832e29b39bc9bb75f9f6aeaa49ee
Larksel <lemuelgames@outlook.com>
2025-05-19 00:44:02 -0300

    :recycle: refactor: Alterado tipo dos campos de data e hora da MarkPointDTO

--- PontoAll.WebAPI/Objects/Dtos/Entities/MarkPointDTO.cs
+++ PontoAll.WebAPI/Objects/Dtos/Entities/MarkPointDTO.cs
@@ -5,8 +5,8 @@ namespace PontoAll.WebAPI.Objects.Dtos.Entities;
 public class MarkPointDTO
 {
     public int Id { get; set; }
-    public string Date { get; set; }
-    public string Time { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
     public Geolocation Location { get; set; }
     public string Photo { get; set; }
     public int UserId { get; set; }
