95f5ea958695637de598ffef570e6c56d89f1e6b
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-20 16:47:58 -0300

    :sparkles: feat: Criando Validação da hora validada, e definindo se tiver errado o horário

---
+++ PontoAll.WebAPI/Objects/Utils/HoraValidaAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PontoAll.WebAPI.Objects.Utils
+{
+    public class HoraValidaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var horaStr = value as string;
+
+            // Permite campos vazios ou nulos
+            if (string.IsNullOrWhiteSpace(horaStr))
+                return ValidationResult.Success;
+
+            if (!TimeOnly.TryParseExact(horaStr, "HH:mm:ss", out var _))
+            {
+                return new ValidationResult(
+                    $"O valor '{horaStr}' em {validationContext.DisplayName} não é um horário válido. Use o formato HH:mm:ss entre 00:00:00 e 23:59:59."
+                );
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
