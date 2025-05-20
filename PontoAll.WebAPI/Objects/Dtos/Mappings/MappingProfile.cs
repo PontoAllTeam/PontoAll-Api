30ccb39ab7b287022018012f294ceda1d144da79
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-20 16:49:58 -0300

    :wrench: chore: Configurando o MappingProfile para converter de string para TimeOnly

--- PontoAll.WebAPI/Objects/Dtos/Mappings/MappingProfile.cs
+++ PontoAll.WebAPI/Objects/Dtos/Mappings/MappingProfile.cs
@@ -2,16 +2,51 @@ using AutoMapper;
 using PontoAll.WebAPI.Objects.Dtos.Entities;
 using PontoAll.WebAPI.Objects.Models;
 
-namespace PontoAll.WebAPI.Objects.Dtos.Mappings;
-
-public class MappingProfile : Profile
+namespace PontoAll.WebAPI.Objects.Dtos.Mappings
 {
-    public MappingProfile()
+    public class MappingProfile : Profile
     {
-        CreateMap<ScaleDTO, Scale>().ReverseMap();
-        CreateMap<CompanyDTO, Company>().ReverseMap();
-        CreateMap<UserDTO, User>().ReverseMap();
-        CreateMap<DepartmentDTO, Department>().ReverseMap();
-        CreateMap<SectorDTO, Sector>().ReverseMap();
+        public MappingProfile()
+        {
+            CreateMap<ScaleDTO, Scale>()
+                .ForMember(dest => dest.Pick1, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick1)))
+                .ForMember(dest => dest.Pick2, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick2)))
+                .ForMember(dest => dest.Pick3, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick3)))
+                .ForMember(dest => dest.Pick4, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick4)))
+                .ForMember(dest => dest.Pick5, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick5)))
+                .ForMember(dest => dest.Pick6, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick6)))
+                .ForMember(dest => dest.Pick7, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick7)))
+                .ForMember(dest => dest.Pick8, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick8)))
+                .ForMember(dest => dest.Pick9, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick9)))
+                .ForMember(dest => dest.Pick10, opt => opt.MapFrom(src => ParseTimeOnly(src.Pick10)));
+
+            CreateMap<Scale, ScaleDTO>()
+                .ForMember(dest => dest.Pick1, opt => opt.MapFrom(src => src.Pick1.HasValue ? src.Pick1.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick2, opt => opt.MapFrom(src => src.Pick2.HasValue ? src.Pick2.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick3, opt => opt.MapFrom(src => src.Pick3.HasValue ? src.Pick3.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick4, opt => opt.MapFrom(src => src.Pick4.HasValue ? src.Pick4.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick5, opt => opt.MapFrom(src => src.Pick5.HasValue ? src.Pick5.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick6, opt => opt.MapFrom(src => src.Pick6.HasValue ? src.Pick6.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick7, opt => opt.MapFrom(src => src.Pick7.HasValue ? src.Pick7.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick8, opt => opt.MapFrom(src => src.Pick8.HasValue ? src.Pick8.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick9, opt => opt.MapFrom(src => src.Pick9.HasValue ? src.Pick9.Value.ToString("HH:mm:ss") : null))
+                .ForMember(dest => dest.Pick10, opt => opt.MapFrom(src => src.Pick10.HasValue ? src.Pick10.Value.ToString("HH:mm:ss") : null));
+
+            CreateMap<CompanyDTO, Company>().ReverseMap();
+            CreateMap<UserDTO, User>().ReverseMap();
+            CreateMap<DepartmentDTO, Department>().ReverseMap();
+            CreateMap<SectorDTO, Sector>().ReverseMap();
+        }
+
+        private static TimeOnly? ParseTimeOnly(string? timeString)
+        {
+            if (string.IsNullOrWhiteSpace(timeString))
+                return null;
+
+            if (TimeOnly.TryParse(timeString, out var time))
+                return time;
+
+            throw new Exception($"Formato inválido para horário: {timeString}");
+        }
     }
 }
