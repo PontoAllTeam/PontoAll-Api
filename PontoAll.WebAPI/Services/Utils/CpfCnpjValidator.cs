0dcb556bc9122f95a45296d287acf09f4ed58111
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-26 12:57:38 -0300

    :wrench: chore: Arrumando a definição das pastas, e arquivos

---
+++ PontoAll.WebAPI/Services/Utils/CpfCnpjValidator.cs
@@ -0,0 +1,40 @@
+﻿using PontoAll.WebAPI.Objects.Dtos.Entities;
+using PontoAll.WebAPI.Services.Utils;
+using PontoAll.WebAPI.Objects.Models;
+
+namespace PontoAll.WebAPI.Services.Utils;
+
+public static class CpfCnpjValidator
+{
+    public static void Validate(UserDTO user)
+    {
+        user.Cpf = StringUtils.ExtractNumbers(user.Cpf);
+
+        if (!IsValidCPF(user.Cpf))
+        throw new Exception("CPF inválido.");
+    }
+    private static bool IsValidCPF(string cpf)
+    {
+        if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+            return false;
+
+        if (new string(cpf[0], 11) == cpf)
+            return false;
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+            sum += (cpf[i] - '0') * (10 - i);
+
+        int firstDigit = sum % 11;
+        firstDigit = firstDigit < 2 ? 0 : 11 - firstDigit;
+
+        sum = 0;
+        for (int i = 0; i < 10; i++)
+            sum += (cpf[i] - '0') * (11 - i);
+
+        int secondDigit = sum % 11;
+        secondDigit = secondDigit < 2 ? 0 : 11 - secondDigit;
+
+        return cpf[9] - '0' == firstDigit && cpf[10] - '0' == secondDigit;
+    }
+}
--- PontoAll.WebAPI/Services/Utils/UserValidator.cs
+++ PontoAll.WebAPI/Services/Utils/UserValidator.cs
@@ -11,41 +11,12 @@ public static class UserValidator
         if (string.IsNullOrWhiteSpace(user.Name))
             throw new Exception("Nome é obrigatório.");
 
-        user.Cpf = StringUtils.ExtractNumbers(user.Cpf);
         user.Phone = StringUtils.ExtractNumbers(user.Phone);
-
-        if (!IsValidCPF(user.Cpf))
-            throw new Exception("CPF inválido.");
 
         if (!PhoneValidator.IsValidPhone(user.Phone))
             throw new Exception("Telefone inválido.");
 
         if (!EmailValidator.IsValidEmail(user.Email))
             throw new Exception("Email inválido.");
-    }
-
-    private static bool IsValidCPF(string cpf)
-    {
-        if (cpf.Length != 11 || !cpf.All(char.IsDigit))
-            return false;
-
-        if (new string(cpf[0], 11) == cpf)
-            return false;
-
-        int sum = 0;
-        for (int i = 0; i < 9; i++)
-            sum += (cpf[i] - '0') * (10 - i);
-
-        int firstDigit = sum % 11;
-        firstDigit = firstDigit < 2 ? 0 : 11 - firstDigit;
-
-        sum = 0;
-        for (int i = 0; i < 10; i++)
-            sum += (cpf[i] - '0') * (11 - i);
-
-        int secondDigit = sum % 11;
-        secondDigit = secondDigit < 2 ? 0 : 11 - secondDigit;
-
-        return cpf[9] - '0' == firstDigit && cpf[10] - '0' == secondDigit;
     }
 }
