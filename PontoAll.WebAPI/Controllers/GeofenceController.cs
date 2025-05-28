f4f9aa453369702211c446f65ac32e55d0e52f85
Larksel <lemuelgames@outlook.com>
2025-05-27 23:32:48 -0300

    :sparkles: feat: Controller básica de Geofence

--- PontoAll.WebAPI/Controllers/GeofenceController.cs
+++ PontoAll.WebAPI/Controllers/GeofenceController.cs
@@ -4,61 +4,60 @@ using Microsoft.AspNetCore.Mvc;
 using PontoAll.WebAPI.Objects.Contracts;
 using PontoAll.WebAPI.Objects.Dtos.Entities;
 using PontoAll.WebAPI.Services.Interfaces;
-using PontoAll.WebAPI.Services.Utils;
 
 namespace PontoAll.WebAPI.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-public class MarkPointController : Controller
+public class GeofenceController : Controller
 {
-    private readonly IMarkPointService _markPointService;
+    private readonly IGeofenceService _geofenceService;
     private readonly Response _response;
 
-    public MarkPointController(IMarkPointService markPointService)
+    public GeofenceController(IGeofenceService geofenceService)
     {
-        _markPointService = markPointService;
+        _geofenceService = geofenceService;
         _response = new Response();
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
-    {
-        var markPointsDTO = await _markPointService.GetAll();
+    {
+        var geofencesDTO = await _geofenceService.GetAll();
 
         _response.Code = ResponseEnum.SUCCESS;
-        _response.Data = markPointsDTO;
-        _response.Message = "Marcações de ponto listadas com sucesso";
+        _response.Data = geofencesDTO;
+        _response.Message = "Cercas virtuais listadas com sucesso";
 
         return Ok(_response);
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
-    {
-        var markPointDTO = await _markPointService.GetById(id);
+    {
+        var geofenceDTO = await _geofenceService.GetById(id);
 
-        if (markPointDTO is null)
+        if (geofenceDTO is null)
         {
             _response.Code = ResponseEnum.NOT_FOUND;
             _response.Data = null;
-            _response.Message = "Marcação de ponto não encontrada";
+            _response.Message = "Cerca virtual não encontrada";
 
             return NotFound(_response);
         }
 
         _response.Code = ResponseEnum.SUCCESS;
-        _response.Data = markPointDTO;
-        _response.Message = "Marcação de ponto listada com sucesso";
+        _response.Data = geofenceDTO;
+        _response.Message = "Cerca virtual listada com sucesso";
 
         return Ok(_response);
     }
 
-    [HttpPost]
-    public async Task<IActionResult> Post(MarkPointDTO markPointDTO)
+    [HttpPost]
+    public async Task<IActionResult> Post(GeofenceDTO geofenceDTO)
     {
-        if (markPointDTO is null)
+        if (geofenceDTO is null)
         {
             _response.Code = ResponseEnum.INVALID;
             _response.Data = null;
@@ -66,30 +65,21 @@ public class MarkPointController : Controller
 
             return BadRequest(_response);
         }
-
-        if (!GeolocationValidator.IsValidGeolocation(markPointDTO.Location))
-        {
-            _response.Code = ResponseEnum.INVALID;
-            _response.Data = null;
-            _response.Message = "Formato das coordenadas de geolocalização incorreto";
-
-            return BadRequest(_response);
-        }
 
         try
         {
-            await _markPointService.Create(markPointDTO);
+            await _geofenceService.Create(geofenceDTO);
 
             _response.Code = ResponseEnum.SUCCESS;
-            _response.Data = markPointDTO;
-            _response.Message = "Marcação de ponto cadastrada com sucesso";
+            _response.Data = geofenceDTO;
+            _response.Message = "Cerca virtual cadastrada com sucesso";
 
             return Ok(_response);
         }
         catch (Exception ex)
         {
             _response.Code = ResponseEnum.ERROR;
-            _response.Message = "Não foi possível cadastrar a marcação de ponto";
+            _response.Message = "Não foi possível cadastrar a cerca virtual";
             _response.Data = new
             {
                 ErrorMessage = ex.Message,
@@ -99,10 +89,10 @@ public class MarkPointController : Controller
         }
     }
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, MarkPointDTO markPointDTO)
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, GeofenceDTO geofenceDTO)
     {
-        if (markPointDTO is null)
+        if (geofenceDTO is null)
         {
             _response.Code = ResponseEnum.INVALID;
             _response.Data = null;
@@ -111,38 +101,29 @@ public class MarkPointController : Controller
             return BadRequest(_response);
         }
 
-        if (!GeolocationValidator.IsValidGeolocation(markPointDTO.Location))
-        {
-            _response.Code = ResponseEnum.INVALID;
-            _response.Data = null;
-            _response.Message = "Formato das coordenadas de geolocalização incorreto";
-
-            return BadRequest(_response);
-        }
-
         try
         {
-            var existingMarkPointDTO = await _markPointService.GetById(id);
-            if (existingMarkPointDTO is null)
+            var existingGeofenceDTO = await _geofenceService.GetById(id);
+            if (existingGeofenceDTO is null)
             {
                 _response.Code = ResponseEnum.NOT_FOUND;
                 _response.Data = null;
-                _response.Message = "A marcação de ponto informada não existe";
+                _response.Message = "A cerca virtual informada não existe";
                 return NotFound(_response);
             }
 
-            await _markPointService.Update(markPointDTO, id);
+            await _geofenceService.Update(geofenceDTO, id);
 
             _response.Code = ResponseEnum.SUCCESS;
-            _response.Data = markPointDTO;
-            _response.Message = "Marcação de ponto atualizada com sucesso";
+            _response.Data = geofenceDTO;
+            _response.Message = "Cerca virtual atualizada com sucesso";
 
             return Ok(_response);
         }
         catch (Exception ex)
         {
             _response.Code = ResponseEnum.ERROR;
-            _response.Message = "Ocorreu um erro ao tentar atualizar a marcação de ponto";
+            _response.Message = "Ocorreu um erro ao tentar atualizar a cerca virtual";
             _response.Data = new
             {
                 ErrorMessage = ex.Message,
@@ -152,32 +133,32 @@ public class MarkPointController : Controller
         }
     }
 
-    [HttpDelete("{id}")]
+    [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
         try
         {
-            var existingMarkPointDTO = await _markPointService.GetById(id);
-            if (existingMarkPointDTO is null)
+            var existingGeofenceDTO = await _geofenceService.GetById(id);
+            if (existingGeofenceDTO is null)
             {
                 _response.Code = ResponseEnum.NOT_FOUND;
                 _response.Data = null;
-                _response.Message = "A marcação de ponto informada não existe";
+                _response.Message = "A cerca virtual informada não existe";
                 return NotFound(_response);
             }
 
-            await _markPointService.Remove(id);
+            await _geofenceService.Remove(id);
 
             _response.Code = ResponseEnum.SUCCESS;
             _response.Data = null;
-            _response.Message = "Marcação de ponto removida com sucesso";
+            _response.Message = "Cerca virtual removida com sucesso";
 
             return Ok(_response);
         }
         catch (Exception ex)
         {
             _response.Code = ResponseEnum.ERROR;
-            _response.Message = "Ocorreu um erro ao tentar remover a marcação de ponto";
+            _response.Message = "Ocorreu um erro ao tentar remover a cerca virtual";
             _response.Data = new
             {
                 ErrorMessage = ex.Message,
